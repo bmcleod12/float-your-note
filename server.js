a7bab1712f84e2755cce2c43c6aca2ab2048eccc
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-02-26 16:44:07 -0600

    configured server properties

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-
+// Dependencies
 const express = require('express');
-
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
 
 // Tells node that we are creating an "express" server
 const app = express();
 
-// Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT || 8080;
+// Sets an initial port
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+// Gives the server ability to load files in the public directory
+app.use(express.static('public'));
 
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-
+// Routes give the server a "map" of how to respond when users visit or request data from various URLs
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
-// LISTENER
-// The below code effectively "starts" our server
-
+// Effectively "starts" the server
 app.listen(PORT, () => {
   console.log(`App listening on PORT: ${PORT}`);
 });
