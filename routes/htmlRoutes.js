214887d03fb233e663cc47e8d494d6f0b0fb7aed
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-02-27 08:56:20 -0600

    html route updated notes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,33 +1,6 @@
-// DEPENDENCIES
-// We need to include the path package to get the correct file path for our html
-
-// const path = require('path');
-
-// ROUTING
-
-// module.exports = (app) => {
-//   // => HTML GET Requests
-//   // Below code handles when users "visit" a page.
-//   // In each of the below cases the user is shown an HTML page of content
-
-//   // app.get('/tables', (req, res) => {
-//   //   res.sendFile(path.join(__dirname, '../public/tables.html'));
-//   // });
-
-//   // app.get('/reserve', (req, res) => {
-//   //   res.sendFile(path.join(__dirname, '../public/reserve.html'));
-//   // });
-
-//   // // If no matching route is found default to home
-//   // app.get('*', (req, res) => {
-//   //   res.sendFile(path.join(__dirname, '../public/home.html'));
-//   // });
-// };
-
-
 const path = require('path');
 
-// Writes paths to HTML pages
+// HTML page routing
 module.exports = (app) => {
     app.get('/notes', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/notes.html'));
@@ -36,7 +9,8 @@ module.exports = (app) => {
     app.get('/', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
-    // In case of no matching route, default to index
+
+    // Catch-all to default to index.html
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
