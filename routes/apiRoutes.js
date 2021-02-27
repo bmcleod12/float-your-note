db8f2249a78c0ec05814cb505d36217ead63b7f8
Brenna McLeod <brenna.mcleod94@gmail.com>
2021-02-27 08:53:30 -0600

    coding for clicked routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,52 +1,66 @@
-// // LOAD DATA
-// // We are linking our routes to a series of "data" sources.
-// // These data sources hold arrays of information on table-data, waitinglist, etc.
+const fs = require('fs');
 
-// const tableData = require('../data/tableData');
-// const waitListData = require('../data/waitinglistData');
+// Gets the API notes already in the db file
+module.exports = (app) => {
+  app.get('/api/notes', (req, res) => {
+      fs.readFile('./db/db.json', (err, data) => {
+          if (err) throw err;
+          notesList = JSON.parse(data);
+          res.send(notesList);
+      });
+  });
 
-// // ROUTING
+  // Posts new notes to the JSON db file
+  app.post('/api/notes', (req, res) => {
+      const userNotes = req.body;
 
-// module.exports = (app) => {
-//   // API GET Requests
-//   // Below code handles when users "visit" a page.
-//   // In each of the below cases when a user visits a link
-//   // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-//   // ---------------------------------------------------------------------------
+      fs.readFile('./db/db.json', (err, data) => {
+          if (err) throw err;
+          notesList = JSON.parse(data);
+          notesList.push(userNotes);
+          notesList.forEach((item, i) => {
+              item.id = i + 1;
+              return notesList;
+          });
+          console.log(notesList);
 
-//   app.get('/api/tables', (req, res) => res.json(tableData));
+          fs.writeFile('./db/db.json', JSON.stringify(notesList), (err, data) => {
+              if (err) throw err;
+          });
+      });
+      res.status(200).send();
+  });
 
-//   app.get('/api/waitlist', (req, res) => res.json(waitListData));
+  // Deletes existing notes from JSON db file
+  app.delete("/api/notes/:id", (req, res) => {
+    var noteID = req.params.id;
 
-//   // API POST Requests
-//   // Below code handles when a user submits a form and thus submits data to the server.
-//   // In each of the below cases, when a user submits form data (a JSON object)
-//   // ...the JSON is pushed to the appropriate JavaScript array
-//   // (ex. User fills out a reservation request... this data is then sent to the server...
-//   // Then the server saves the data to the tableData array)
-//   // ---------------------------------------------------------------------------
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      notesList = JSON.parse(data);
+      
+      // Checks each ID to see if it matches the one selected by the user, then removes that note and reassigns the IDs
+      for(let i = 0; i < notesList.length; i++) {
+        if (notesList[i].id === Number(noteID)) {
+          notesList.splice([i], 1);
+        }
+      }
 
-//   app.post('/api/tables', (req, res) => {
-//     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-//     // It will do this by sending out the value "true" have a table
-//     // req.body is available since we're using the body parsing middleware
-//     if (tableData.length < 5) {
-//       tableData.push(req.body);
-//       res.json(true);
-//     } else {
-//       waitListData.push(req.body);
-//       res.json(false);
-//     }
-//   });
+      // Shifts the note objects and gives updated IDs
+      notesList.forEach((item, i) => {
+        item.id = i + 1;
+      });
 
-//   // I added this below code so you could clear out the table while working with the functionality.
-//   // Don"t worry about it!
+      console.log(notesList);
 
-//   app.post('/api/clear', (req, res) => {
-//     // Empty out the arrays of data
-//     tableData.length = 0;
-//     waitListData.length = 0;
+      fs.writeFile('./db/db.json', JSON.stringify(notesList), (err, data) => {
+        if (err) throw err;
+      });
+  
+    res.status(204).send();
 
-//     res.json({ ok: true });
-//   });
-// };
+  });
+  });
+
+  
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,43 @@
-// // DEPENDENCIES
-// // We need to include the path package to get the correct file path for our html
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
 
 // const path = require('path');
 
-// // ROUTING
+// ROUTING
 
 // module.exports = (app) => {
 //   // => HTML GET Requests
 //   // Below code handles when users "visit" a page.
 //   // In each of the below cases the user is shown an HTML page of content
 
-//   app.get('/tables', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/tables.html'));
-//   });
+//   // app.get('/tables', (req, res) => {
+//   //   res.sendFile(path.join(__dirname, '../public/tables.html'));
+//   // });
 
-//   app.get('/reserve', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/reserve.html'));
-//   });
+//   // app.get('/reserve', (req, res) => {
+//   //   res.sendFile(path.join(__dirname, '../public/reserve.html'));
+//   // });
 
-//   // If no matching route is found default to home
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, '../public/home.html'));
-//   });
+//   // // If no matching route is found default to home
+//   // app.get('*', (req, res) => {
+//   //   res.sendFile(path.join(__dirname, '../public/home.html'));
+//   // });
 // };
+
+
+const path = require('path');
+
+// Writes paths to HTML pages
+module.exports = (app) => {
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
+    });
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+    // In case of no matching route, default to index
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
+    });
+}
